1a63d9e1e3ad4af71bc64cae42d97cd2f7c085b2
JH Bae <mastropseudo@gmail.com>
2025-01-20 14:51:26 +0100

    Enhance Licensing and Contribution Guidelines

--- include/jh/immutable_str.h
+++ include/jh/immutable_str.h
@@ -1,4 +1,21 @@
 /**
+ * Copyright 2025 JeongHan-Bae <mastropseudo@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
  * @file immutable_str.h
  * @author JeongHan-Bae <mastropseudo@gmail.com>
  * @brief A lightweight immutable string with optional automatic trimming.
@@ -43,7 +60,7 @@
  * - **Seamless C-string Compatibility**: Provides `c_str()`[const char*], `view()`[std::string_view()],
  * and `str()`[std::string].
  *
- * @version 1.0
+ * @version 1.0.x
  * @date 2025
  */
 
